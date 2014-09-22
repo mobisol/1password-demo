38ea523237688e22a2fad1195a6919676b591e00
Dmitry Beloborodov <dmi3j@me.com>
2014-09-22 15:09:10 +0300

    Simple implementation of integration with 1Password based on custom URL scheme support.

--- 1password-demo/ViewController.m
+++ 1password-demo/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *checkResultLabel;
+@property (weak, nonatomic) IBOutlet UITextField *passKeyword;
+@property (weak, nonatomic) IBOutlet UIButton *searchPassButton;
 
 @end
 
@@ -23,5 +26,32 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+// code from http://blog.agilebits.com/2013/01/24/developers-heres-how-to-add-a-little-1password-to-your-ios-apps/
+
+- (IBAction)checkPressed:(id)sender
+{
+    self.checkResultLabel.hidden = NO;
+
+    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"onepassword://search"]]) {
+        NSLog(@"1Password is installed!");
+        self.checkResultLabel.textColor = [UIColor greenColor];
+        self.checkResultLabel.text = @"1Password is installed!";
+        self.searchPassButton.enabled =
+        self.passKeyword.enabled = YES;
+    } else {
+        self.checkResultLabel.textColor = [UIColor redColor];
+        self.checkResultLabel.text = @"1Password is NOT installed";
+        self.searchPassButton.enabled =
+        self.passKeyword.enabled = NO;
+    }
+}
+
+- (IBAction)searchPassPressed:(id)sender
+{
+    NSString *passKey = [self.passKeyword.text stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"onepassword://search/%@", passKey]]];
+}
+
 
 @end
